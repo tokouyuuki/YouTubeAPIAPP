5b31b93c14fb4d6016f9233ee03648969a80205a
tokouyuuki <tttyuki1050@gmail.com>
2022-03-14 22:31:38 +0900

    プロジェクトの作成

---
+++ YoutubeAPIAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeAPIAPP
+//
+//  Created by 都甲裕希 on 2022/03/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
