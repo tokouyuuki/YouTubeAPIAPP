c867f42b291cf599175d7eb149338f2cdc008fae
tokouyuuki <tttyuki1050@gmail.com>
2022-03-15 04:45:36 +0900

    新規ファイルの作成

---
+++ YoutubeAPIAPP/VideoListViewController/VideoListPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  VideoListPresenter.swift
+//  YoutubeAPIAPP
+//
+//  Created by 都甲裕希 on 2022/03/15.
+//
+
+import Foundation
---
+++ YoutubeAPIAPP/VideoListViewController/VideoListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VideoListViewController.swift
+//  YoutubeAPIAPP
+//
+//  Created by 都甲裕希 on 2022/03/15.
+//
+
+import UIKit
+
+class VideoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
