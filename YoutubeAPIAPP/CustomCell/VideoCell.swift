b3f7befcaae3751b5fe54bf08ab085bda861606a
tokouyuuki <tttyuki1050@gmail.com>
2022-03-15 18:51:12 +0900

    CustomCellファイルの作成

---
+++ YoutubeAPIAPP/CustomCell/VideoCell.swift
@@ -0,0 +1,23 @@
+//
+//  VideoCell.swift
+//  YoutubeAPIAPP
+//
+//  Created by 都甲裕希 on 2022/03/15.
+//
+
+import UIKit
+
+class VideoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
